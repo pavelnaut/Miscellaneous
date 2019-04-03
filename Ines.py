fba5699b54e29d449feea3800b6d6899632acacb
Bad-Wolf <prince.of.persia@abv.bg>
2019-04-04 00:47:01 +0300

    Added dictionary for student qualities
    While loop is now conditional for teacher's name

--- Ines.py
+++ Ines.py
@@ -5,6 +5,7 @@ class Human:
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
+        self.full_name = f"{self.first_name} {self.last_name}"
  
  
 class Teacher(Human):
@@ -21,17 +22,22 @@ class DreamTeacher(Teacher):
         self.charm = charm
  
     def __str__(self):
-        return f"Thank you for everything {self.first_name}!"
+        return f"Thank you for being the best, {self.first_name}!"
  
  
-while True:
+student = {'fun': 0, 'knowledge': 0, 'inspiration': 0}
  
-    my_teacher = DreamTeacher('Ines', 'Ivanova', sys.maxsize, sys.maxsize, sys.maxsize, sys.maxsize)
+my_teacher = DreamTeacher('Ines', 'Ivanova', sys.maxsize, sys.maxsize, sys.maxsize, sys.maxsize)
+ 
+while my_teacher.full_name == 'Ines Ivanova':
+ 
+    for quality in student.keys():
+        student[quality] += 1
  
     try:
         print(my_teacher)
     except:
-        print(f"Stop kidding me. There is no way to raise exception in this case!")
+        print("Stop kidding me. There is no way to raise exception in this case!")
  
     next_lesson = eval(input())
-    # TODO!
+    # TODO MORE LESSONS!
