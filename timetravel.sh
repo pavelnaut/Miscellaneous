8e44e830a4c7bc1348bfae37ba365ec779957517
pavelnaut <pavelmbogdanov@gmail.com>
2023-03-15 00:44:52 +0200

    Change default submodule behaviour

--- timetravel.sh
+++ timetravel.sh
@@ -1,15 +1,21 @@
 commit_message=${1}
 time_travel_days=${2:-0}
-commit_submodule=${3:-css-exercises}
+submodule_name=${3:-0}
 
 # current date minus time_travel_days
 commit_date=$(date -d "$date - $time_travel_days days")
 echo Commit as $commit_date
-cd $commit_submodule
+
+# if change is inside submodule 
+if [ $# -eq 3 ]; then
+    cd $submodule_name
+    git add .
+    GIT_COMMITTER_DATE="$commit_date" git commit --date "$commit_date" -m "$commit_message"
+    git push origin main
+    cd ../
+fi
+
 git add .
 GIT_COMMITTER_DATE="$commit_date" git commit --date "$commit_date" -m "$commit_message"
 git push origin main
-cd ../
-git add .
-GIT_COMMITTER_DATE="$commit_date" git commit --date "$commit_date" -m "$commit_message"
-git push origin main
+
